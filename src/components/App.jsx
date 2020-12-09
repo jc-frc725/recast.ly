d4091c3800ccb2a355b951f14981a7518e9f894d
Justin Chow <hrll71990@gmail.com>
2020-12-08 20:11:49 -0800

    VideoPlayer component now dynamically renders a video from exampleVidData.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 var App = () => (
@@ -9,7 +10,7 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <VideoPlayer video={exampleVideoData[0]} />
       </div>
       <div className="col-md-5">
         <VideoList videos={exampleVideoData} />
