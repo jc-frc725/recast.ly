9b4ccc8efdbc05e04ef9a7c8b5f674f3e111418e
Justin Chow <hrll71990@gmail.com>
2020-12-08 18:24:41 -0800

    refactored VideoList to dynamically render one VideoListEntry componenet for each video object in exampleVidData.js

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,12 @@
 import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
+  // props.videos is the array of video data
+  // have a function that iterates thru props.videos for each video data
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map(video =>
+      <VideoListEntry entry={video} />
+    )}
   </div>
 );
 
