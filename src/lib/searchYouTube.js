a627cbbecbbed5a8ba318020b1972ea70318d7ae
Justin Chow <hrll71990@gmail.com>
2020-12-09 01:56:40 -0800

    possibly implemented searchYoutube.js, appears to be fetching video data in correct format from Youtube API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,34 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  // TODO:
+
+  // 'options' is an object w/
+  // query: 'string to search for'
+  // max: maximum # of vids to get, defaults to 5 if no max given
+  // key: authorized Youtube Data API Key
+  options.max = options.max || 5;
+
+  // use jQuery to send GET request to Youtube's Search:list endpoint
+  // should only GET embeddable videos
+  $.get({
+    url:'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      part: ['snippet'],
+      videoEmbeddable: ['true'],
+      type: 'video'
+    },
+    success: (data) => {
+      //console.log(data);
+      callback(data.items);
+    },
+    error: (data) => {
+      console.error('Something went wrong. ' + data);
+    },
+  });
+  // invoke 'callback' with videos array that Youtube gives back from hitting endpoint
+
 };
 
 export default searchYouTube;
